52d95341192ea2e97c6978ef67a9e50f4f4ff6b3
Ex333 <haldas.mateusz@gmail.com>
2026-04-17 09:32:59 +0200

    Added model Barber and Services

--- barber/models.py
+++ barber/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Barber(models.Model):
+    name = models.CharField(max_length=64)
+    bio = models.TextField(blank=True)
+    age = models.PositiveSmallIntegerField()
+    photo = models.ImageField(upload_to='barbers/')
+
+    def __str__(self):
+        return self.name
+    
+class Services(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits=6, decimal_places= 2 )
+    duration = models.PositiveSmallIntegerField(help_text="Time in minutes")
+
+    barbers = models.ManyToManyField(Barber, related_name="services")
+
+    def __str__(self):
+        return f"{self.name} - {self.price} zł"
