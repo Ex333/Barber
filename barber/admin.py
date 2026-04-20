d074e243ae1c0cae32497a8e15836745262479a5
Ex333 <haldas.mateusz@gmail.com>
2026-04-20 19:49:57 +0200

    Added to the admin

--- barber/admin.py
+++ barber/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Barber, Service, Booking
 
 # Register your models here.
+@admin.register(Barber)
+class BarberAdmin(admin.ModelAdmin):
+    list_display = ("name", "age")
+    search_fields = ('name', )
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "duration")
+    filter_horizontal = ("barbers",)
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("customer_name","service", "barber", "date", "time")
+    list_filter = ("date", "barber")
+    search_fields = ("customer_name", "costomer_email")
+
+
